caaea6ec199f832cff6ddf916c9922a371e34931
axell-brendow <breno.axel@gmail.com>
2021-09-22 00:57:03 -0300

    Add example of Mono.defer

--- src/main/java/br/com/axellbrendow/App.java
+++ src/main/java/br/com/axellbrendow/App.java
@@ -9,6 +9,8 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class App 
 {
+    private static int a = 5;
+
     public static void main( String[] args )
     {
         // Mono.just("Hello World!")
@@ -29,12 +31,26 @@ public class App
         //     .repeat()
         //     .subscribe(v -> log.info("just or empty: {}", v));
 
-        Flux.from(Mono.error(new RuntimeException("test")))
-            .repeat()
-            .subscribe(
-                v -> log.info("just or empty: {}", v),
-                error -> log.error("Error: {}", error),
-                () -> log.info("Completed")
-            );
+        // Flux.from(Mono.error(new RuntimeException("test")))
+        //     .repeat()
+        //     .subscribe(
+        //         v -> log.info("just or empty: {}", v),
+        //         error -> log.error("Error: {}", error),
+        //         () -> log.info("Completed")
+        //     );
+
+        // Subscribers of this mono will always get the initial value of a
+        var just = Mono.just(a);
+
+        // Mono.defer will generate a new Mono.just for each subscriber rerunning the lambda function
+        var defer = Mono.defer(() -> Mono.just(a));
+
+        just.subscribe(v -> System.out.println("just: " + v));
+        defer.subscribe(v -> System.out.println("defer: " + v));
+
+        a = 7;
+
+        just.subscribe(v -> System.out.println("just: " + v));
+        defer.subscribe(v -> System.out.println("defer: " + v));
     }
 }
