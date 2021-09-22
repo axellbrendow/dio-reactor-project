12aff94d0217c421cb7f034473a08c03aa348da5
axell-brendow <breno.axel@gmail.com>
2021-09-22 00:24:41 -0300

    Add Flux example

--- src/main/java/br/com/axellbrendow/App.java
+++ src/main/java/br/com/axellbrendow/App.java
@@ -1,6 +1,7 @@
 package br.com.axellbrendow;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -8,7 +9,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        Mono.just("Hello World!")
-            .subscribe(log::info);
+        // Mono.just("Hello World!")
+        //     .subscribe(log::info);
+
+        Flux.just(1, 2, 3, 4, 5)
+            .subscribe(i -> log.info("num: {}", i));
     }
 }
