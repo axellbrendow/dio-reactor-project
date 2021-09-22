aca8132beaa638dd4f9537fbd261b3c550e09a0a
axell-brendow <breno.axel@gmail.com>
2021-09-22 00:33:44 -0300

    Add example of a flux of nullable values

--- src/main/java/br/com/axellbrendow/App.java
+++ src/main/java/br/com/axellbrendow/App.java
@@ -1,5 +1,7 @@
 package br.com.axellbrendow;
 
+import java.util.Optional;
+
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,8 +17,16 @@ public class App
         // Flux.just(1, 2, 3, 4, 5)
         //     .subscribe(i -> log.info("num: {}", i));
 
-        Flux.range(1, 5)
+        // Flux.range(1, 5)
+        //     .repeat()
+        //     .subscribe(i -> log.info("num: {}", i));
+
+        // Flux.from(Mono.justOrEmpty(Optional.ofNullable(null)))
+        //     .repeat()
+        //     .subscribe(v -> log.info("just or empty: {}", v));
+
+        Flux.from(Mono.justOrEmpty(Optional.ofNullable("Hello World")))
             .repeat()
-            .subscribe(i -> log.info("num: {}", i));
+            .subscribe(v -> log.info("just or empty: {}", v));
     }
 }
